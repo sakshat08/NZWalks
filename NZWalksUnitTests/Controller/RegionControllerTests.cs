e1cab491a43051e7b0bb12119dcfff010ddd57f1
Sakshat <sakshatetc08.ss@gmail.com>
2023-12-16 12:59:16 +0530

    pipeline checking passinbg

---
+++ NZWalksUnitTests/Controller/RegionControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NZWalks.API.Controllers;
+using NZWalks.API.Repository;
+using Xunit;
+
+namespace NZWalksUnitTests.Controller
+{
+    public class RegionControllerTests
+    {
+        private readonly Mock<IRegionRepository> mockrepo;
+        private RegionController controller;
+
+        public RegionControllerTests()
+        {
+            this.mockrepo = new Mock<IRegionRepository>();
+            this.controller = new RegionController(this.mockrepo.Object);
+        }
+        
+        [Fact]
+        public async void ControllerReturnNotFoundWhenNoRegionDataPresent()
+        {
+            ////this.mockrepo
+            ////    .Setup(x => x.GetRegionById(It.IsAny<Guid>())).Returns(null);
+            ///
+
+            var actual = await this.controller.GetRegionById(new Guid());
+            actual.Should().BeOfType<NotFoundObjectResult>();
+        }
+    }
+}
